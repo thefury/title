6ee50eb381bc06b25406f4e0bf2ad5fcdc99c1e7
Trevor Oke <trevor@trevoroke.com>
2024-07-26 12:51:21 -0400

    fix: Handle STDIN
    This commit now handles stdin correctly.

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
-	"flag"
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
 
-	"github.com/your-username/title/internal/titlecase"
+	"github.com/thefury/title/internal/titlecase"
+
+	"golang.org/x/term"
 )
 
 func main() {
-	flag.Parse()
-	args := flag.Args()
-
-	if len(args) == 0 {
-		fmt.Println("Please provide a string to convert to title case.")
+	if len(os.Args) > 1 {
+		args := os.Args[1:]
+		fmt.Println(titlecase.ToTitleCase(strings.Join(args, " ")))
+	} else if !term.IsTerminal(int(os.Stdin.Fd())) {
+		// Handle input from standard input
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			fmt.Println(titlecase.ToTitleCase(scanner.Text()))
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Fprintln(os.Stderr, "No input provided. Please provide input as arguments or through stdin.")
 		os.Exit(1)
 	}
-
-	input := strings.Join(args, " ")
-	titleCase := titlecase.ToTitleCase(input)
-	fmt.Println(titleCase)
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/your-username/title
+module github.com/thefury/title
 
 go 1.16
 
-require golang.org/x/text v0.16.0
+require (
+	golang.org/x/term v0.22.0
+	golang.org/x/text v0.16.0
+)
